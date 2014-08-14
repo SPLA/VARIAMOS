94ac3fa4777e12c32d94c79da24e90956345fc04
lufe089 <lufe089@gmail.com>
2014-08-13 22:34:43 -0500

    Defect analyzer refactor and rename of core projects

--- com.cfm.productline.defectAnalyzer/pl/causeAnalyzer/SWI_PROLOGCelularesSplotModelUnsatisfiable.pl
+++
@@ -1,9 +0,0 @@
-:-use_module(library(clpfd)).
-productline(L):-
-L=[Celular,Utilidades,LLamadas,Mensajes,MSN,SMS,
-Juegos,Configuracion,SO,Soporte_Java,Multimedia,MP3,MP4,Camara,VGA,Mexapixeles],
-L ins 0..1,
-Mexapixeles#= 1,
-Celulares#<==>Configuracion,
-Configuracion#<==>SO,
-label(L).
--- com.cfm.productline.defectAnalyzer/src/com/cfm/productline/model/enums/NotationType.java
+++
@@ -1,7 +0,0 @@
-package com.cfm.productline.model.enums;
-
-public enum NotationType {
-
-	FEATURES_MODELS, PRODUCT_LINE,PROLOG;
-
-}
