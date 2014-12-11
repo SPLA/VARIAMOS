f25fdd23d5fcb8265bdee8514dc9ba1b36e885e1
lufe089 <lufe089@gmail.com>
2014-12-11 17:35:32 -0500

    Se elimina codigo innecesario para el calculo de los subsets sobre los que se identifican las correciones

--- com.variamos.defectAnalyzer/src/com/variamos/defectAnalyzer/util/PowerSetUtil.java
+++ com.variamos.defectAnalyzer/src/com/variamos/defectAnalyzer/util/PowerSetUtil.java
@@ -9,66 +9,6 @@ import com.cfm.productline.Constraint;
 
 public class PowerSetUtil {
 
-	public static List<List<String>> formarConjuntoPotenciaString(
-			List<String> conjuntoAnalizar) {
-
-		// Si la lista de elementos solo le queda un elemento en ese caso
-		// retorna la lista que con toda seguridad solo tiene un elemento
-		if (conjuntoAnalizar.size() == 1) {
-			List<List<String>> listaResultado = new ArrayList<List<String>>();
-			listaResultado.add(conjuntoAnalizar);
-			return listaResultado;
-		} else {
-
-			int ultimaPosicion = (conjuntoAnalizar.size() - 1);
-			// Se obtiene el último elemento de los elementos del conjunto. Este
-			// elemento luego será adicionado al conjunto potencia que tiene el
-			// resultado
-			String elementoAnalizar = conjuntoAnalizar.get(ultimaPosicion);
-			// Se quita este elemento del conjunto y nuevamente se llama la
-			// función recursiva, así el primer elemento que se analizar es el
-			// primer elemento de la lista que se envía
-			conjuntoAnalizar.remove(ultimaPosicion);
-
-			// Esta lista de listas almacenará el conjunto resultado de la
-			// recursión, y
-			// será el conjunto que se retorne al final del método con el
-			// conjunto potencia
-			List<List<String>> conjuntoPotencia = formarConjuntoPotenciaString(conjuntoAnalizar);
-
-			// Conjunto de conjuntos que se va a incrementando por cada
-			// iteración (iniciando siempre vacío) para construir con el
-			// elemento que se esta analizando más los anteriores, los posibles
-			// futuros conjuntos. Por ejemplo, si se tiene ya como resultado de
-			// una recursión el conjuntoPotencia {{A}} y se esta analizando el
-			// elemento B, el nuevo conjunto temporal es {{A,B}}
-			List<List<String>> conjuntoTemporal = new ArrayList<List<String>>();
-			for (List<String> elementosConjunto : conjuntoPotencia) {
-				List<String> nuevoConjuntoDelConjuntoPotencia = new ArrayList<String>();
-				nuevoConjuntoDelConjuntoPotencia.addAll(elementosConjunto);
-				nuevoConjuntoDelConjuntoPotencia.add(elementoAnalizar);
-				conjuntoTemporal.add(nuevoConjuntoDelConjuntoPotencia);
-
-			}
-			// Se adiciona al conjunto potencia, un conjunto de un solo elemento
-			// con el elemento que se está analizando en esta pasada recursiva
-			// Por ejeemplo si el conjunto potencia es {{A}} , y se esta
-			// analizando el elemento B, se crea un conjunto de un solo elemento
-			// {B} y luego se adiciona al conjunto potencia quedando este como
-			// {{A}, {B}},
-			List<String> conjuntoElementoAnalizado = new ArrayList<String>();
-			conjuntoElementoAnalizado.add(elementoAnalizar);
-			conjuntoPotencia.add(conjuntoElementoAnalizado);
-
-			// Se adiciona el resultado del conjunto temporal a lo que se
-			// tuviera del conjunto potencia, por ejemplo si el conjunto
-			// potencia tiene el elemento {{A}, {B}}, y el conjunto temporal es
-			// {{A,B}}, el nuevo conjunto potencia es {{A}, {B}, {A,B}}
-			conjuntoPotencia.addAll(conjuntoTemporal);
-			return conjuntoPotencia;
-		}
-
-	}
 
 	/**
 	 * Crea las combinaciones de k elementos de un conjunto de tamaño n
@@ -121,6 +61,10 @@ public class PowerSetUtil {
 
 	/**
 	 * Invoca el algoritmo que hace las combinaciones válidas.
+	 * 
+	 * Construye las combinaciones de tamaño r de elementos que no son
+	 * superconjuntos de los conjuntos bloqueados, ni superconjuntos de los
+	 * conjuntos no permitidos
 	 * @param prefix
 	 * @param elements
 	 * @param resultList
@@ -129,7 +73,7 @@ public class PowerSetUtil {
 	 * @param notAllowedCollectionOfSets
 	 * @return
 	 */
-	public static <E> List<List<E>> externalDoCombinations(List<E> prefix,
+	public static <E> List<List<E>> calculateSets(List<E> prefix,
 			List<E> elements, List<List<E>> resultList, int r,
 			List<List<E>> notAllowedListSuperSets,
 			List<List<E>> notAllowedCollectionOfSets) {
@@ -290,205 +234,6 @@ public class PowerSetUtil {
 
 	}
 
-	/**
-	 * Construye las combinaciones de tamaño r de elementos que no son
-	 * superconjuntos de los conjuntos bloqueados, ni superconjuntos de los
-	 * conjuntos no permitidos
-	 * 
-	 * @param prefix
-	 * @param inputSet
-	 * @param listaResultado
-	 * @param r
-	 * @param notAllowedListSuperSets
-	 * @param notAllowedCollectionOfSets
-	 * @return
-	 */
-	public static <E> List<List<E>> doCombinationsWithoutblockedsubsets(
-			List<E> prefix, List<E> inputSet, List<List<E>> listaResultado,
-			int r, List<List<E>> notAllowedListSuperSets,
-			List<List<E>> notAllowedCollectionOfSets) {
-
-		List<List<E>> possibleMCSToVerify = new ArrayList<List<E>>();
-		List<E> complementBloquedSet = new ArrayList<E>();
-		for (List<E> bloquedSet : notAllowedCollectionOfSets) {
-			complementBloquedSet.clear();
-			complementBloquedSet.addAll(inputSet);
-			complementBloquedSet.removeAll(bloquedSet);
-
-			List<List<E>> partialMCS = new ArrayList<List<E>>();
-			partialMCS = doCombinations(new ArrayList<E>(),
-					complementBloquedSet, partialMCS, notAllowedListSuperSets,
-					r);
-
-			if (!partialMCS.isEmpty()) {
-				possibleMCSToVerify.addAll(partialMCS);
-			}
-
-			// Se itera desde i=1 hasta i=k-1
-			for (int i = 1; i < r; i++) {
-				// La cantidad de elementos de la combinatoria que se toman del
-				// conjunto no bloqueado
-				int j = r - i;
-				List<List<E>> conjuntoElementosNoBloqueados = new ArrayList<List<E>>();
-				conjuntoElementosNoBloqueados = doCombinations(
-						new ArrayList<E>(), complementBloquedSet,
-						conjuntoElementosNoBloqueados, notAllowedListSuperSets,
-						j);
-
-				// Se construye el conjunto con los elementos bloqueados de
-				// tamaño i
-				List<List<E>> conjuntoElementosSiBloqueados = new ArrayList<List<E>>();
-				conjuntoElementosSiBloqueados = doCombinations(
-						new ArrayList<E>(), bloquedSet,
-						conjuntoElementosSiBloqueados, notAllowedListSuperSets,
-						i);
-
-				// Por cada elemento del conjuno de elementos no bloqueados se
-				// adiciona uno del elemento del conjunto de si bloqueados
-				if (!conjuntoElementosSiBloqueados.isEmpty()
-						&& !conjuntoElementosNoBloqueados.isEmpty()) {
-					for (List<E> noBloquedSet : conjuntoElementosNoBloqueados) {
-
-						for (List<E> bloquedSet2 : conjuntoElementosSiBloqueados) {
-							List<E> possibleMCS = new ArrayList<E>();
-							possibleMCS.addAll(noBloquedSet);
-							possibleMCS.addAll(bloquedSet2);
-							if (!SetUtil.verifySetIsSuperSetOfCollectionSets(
-									possibleMCS, notAllowedListSuperSets)
-									&& !SetUtil
-											.<E> verifySetIsSubSetOfCollectionSets(
-													possibleMCS,
-													notAllowedCollectionOfSets)
-									&& !SetUtil.verifyExistSetInSetofSets(
-											possibleMCS, possibleMCSToVerify)) {
-								possibleMCSToVerify.add(possibleMCS);
-							}
-						}
-					}
-				}
-
-			}
-
-		}
-		return possibleMCSToVerify;
-
-	}
-
-	/**
-	 * 
-	 * Para probarlo: System.out.println("PowerSet" +
-	 * constraintPowerSet.toString()); int N = 4; int k = 2; String elements =
-	 * "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	 * 
-	 * @param prefix
-	 * @param elements
-	 * @param r
-	 */
-	public static void generate(String prefix, String elements, int r) {
-		if (r == 0) {
-			System.out.println(prefix);
-			return;
-		}
-		for (int i = 0; i < elements.length(); i++)
-			generate(prefix + elements.charAt(i), elements.substring(i + 1),
-					r - 1);
-	}
-
-	/**
-	 * Construye el conjunto potencia de una lista dada sin incluir el conjunto
-	 * vacío. Si se tiene el conjunto {A,B,C}, el orden en el que se formará
-	 * este conjunto será: {{A},{B},{AB},{C},{AC},{BC},{ABC}}
-	 * 
-	 * @param inputSet
-	 * @return Conjunto potencia sin el conjunto vacío.
-	 */
-	public static <E> List<List<E>> makePowerSet(List<E> inputSet) {
-
-		// Si la lista de elementos solo le queda un elemento en ese caso
-		// retorna la lista que con toda seguridad solo tiene un elemento
-		if (inputSet.size() == 1) {
-			List<List<E>> listaResultado = new ArrayList<List<E>>();
-			listaResultado.add(inputSet);
-			return listaResultado;
-		} else if (inputSet.size() > 1) {
-
-			int ultimaPosicion = (inputSet.size() - 1);
-			// Se obtiene el último elemento de los elementos del conjunto. Este
-			// elemento luego será adicionado al conjunto potencia que tiene el
-			// resultado
-			E elementoAnalizar = inputSet.get(ultimaPosicion);
-			// Se quita este elemento del conjunto y nuevamente se llama la
-			// función recursiva, así el primer elemento que se analizar es el
-			// primer elemento de la lista que se envía
-			inputSet.remove(ultimaPosicion);
-
-			// Esta lista de listas almacenará el conjunto resultado de la
-			// recursión, y
-			// será el conjunto que se retorne al final del método con el
-			// conjunto potencia
-			List<List<E>> conjuntoPotencia = makePowerSet(inputSet);
-
-			// Conjunto de conjuntos que se va a incrementando por cada
-			// iteración (iniciando siempre vacío) para construir con el
-			// elemento que se esta analizando más los anteriores, los posibles
-			// futuros conjuntos. Por ejemplo, si se tiene ya como resultado de
-			// una recursión el conjuntoPotencia {{A}} y se esta analizando el
-			// elemento B, el nuevo conjunto temporal es {{A,B}}
-			List<List<E>> conjuntoTemporal = new ArrayList<List<E>>();
-			for (List<E> elementosConjunto : conjuntoPotencia) {
-				List<E> nuevoConjuntoDelConjuntoPotencia = new ArrayList<E>();
-				nuevoConjuntoDelConjuntoPotencia.addAll(elementosConjunto);
-				nuevoConjuntoDelConjuntoPotencia.add(elementoAnalizar);
-				conjuntoTemporal.add(nuevoConjuntoDelConjuntoPotencia);
-
-			}
-			// Se adiciona al conjunto potencia, un conjunto de un solo elemento
-			// con el elemento que se está analizando en esta pasada recursiva
-			// Por ejeemplo si el conjunto potencia es {{A}} , y se esta
-			// analizando el elemento B, se crea un conjunto de un solo elemento
-			// {B} y luego se adiciona al conjunto potencia quedando este como
-			// {{A}, {B}},
-			List<E> conjuntoElementoAnalizado = new ArrayList<E>();
-			conjuntoElementoAnalizado.add(elementoAnalizar);
-			conjuntoPotencia.add(conjuntoElementoAnalizado);
-
-			// Se adiciona el resultado del conjunto temporal a lo que se
-			// tuviera del conjunto potencia, por ejemplo si el conjunto
-			// potencia tiene el elemento {{A}, {B}}, y el conjunto temporal es
-			// {{A,B}}, el nuevo conjunto potencia es {{A}, {B}, {A,B}}
-			conjuntoPotencia.addAll(conjuntoTemporal);
-			return conjuntoPotencia;
-		}
-		return new ArrayList<List<E>>();
-
-	}
-
-	/**
-	 * Elimina del power set los elementos que contengan la mismas relaciones
-	 * que ya dieron como resultado alguna solución, pues estos ya no se deben
-	 * ser tenidos encuenta como posibles elementos de relajación, al mejorar la
-	 * semántica del modelo y solucionar el defecto
-	 */
-	public List<Constraint> podarPowerSet(
-			List<List<Constraint>> constraintPowerSet,
-			List<Constraint> causeConstraint) {
-
-		List<Constraint> constraintToPrune = new ArrayList<Constraint>();
-		for (List<Constraint> relaxedConstraintList : constraintPowerSet) {
-			boolean podarConstraint = true;
-			uno: for (int i = 0; i < causeConstraint.size(); i++) {
-				Constraint constraint = causeConstraint.get(i);
-				if (!relaxedConstraintList.contains(constraint)) {
-					podarConstraint = false;
-					break uno;
-				}
-			}
-			if (podarConstraint) {
-				constraintToPrune.addAll(relaxedConstraintList);
-
-			}
-		}
-		return constraintToPrune;
-	}
+	
 
 }
