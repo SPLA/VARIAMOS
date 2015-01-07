b04b41bed6e0fb3157b17474707c9e4efe296613
jcmunoz <jcmunoz@jcmunoz-pc.icesi.edu.co>
2015-01-06 23:07:57 -0500

    MIssing class

---
+++ com.variamos.gui/src/com/variamos/gui/refas/editor/actions/VerificationAction.java
@@ -0,0 +1,27 @@
+package com.variamos.gui.refas.editor.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JOptionPane;
+
+import com.mxgraph.util.mxResources;
+import com.variamos.gui.maineditor.BasicGraphEditor;
+import com.variamos.gui.maineditor.AbstractEditorAction;
+import com.variamos.gui.maineditor.VariamosGraphEditor;
+import com.variamos.gui.refas.editor.RefasGraph;
+import com.variamos.refas.core.simulationmodel.Refas2Hlcl;
+
+@SuppressWarnings("serial")
+public class VerificationAction extends AbstractEditorAction {
+
+	public VerificationAction() {
+	}
+
+	/**
+		 * 
+		 */
+	public void actionPerformed(ActionEvent e) {
+		VariamosGraphEditor editor = getEditor(e);
+		editor.verifyErrors();
+	}
+}
