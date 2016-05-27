aef15e1c2b46d12ffe5f8cd2ac7a41bacd8829cd
jcmunozf <jcmunoz@icesi.edu.co>
2016-05-27 15:59:41 -0500

    FIX: missing class

---
+++ com.variamos.dynsup/src/com/variamos/dynsup/statictypes/SatisfactionType.java
@@ -0,0 +1,10 @@
+package com.variamos.dynsup.statictypes;
+
+/* @author Juan C. Muñoz Fernández <jcmunoz@gmail.com>
+ * 
+ * @version 1.1
+ * @since 2014-12-02
+ */
+public enum SatisfactionType {
+	achieve, maintain, optimize, avoid, cease
+}
