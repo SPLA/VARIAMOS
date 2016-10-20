0107497d55333fcaeb593497ff5e5cad1deb0189
jcmunozf <jcmunoz@icesi.edu.co>
2016-10-20 10:37:43 -0500

    BUILD 10-20-16

--- com.variamos.gui/src/com/variamos/gui/maineditor/MainFrame.java
+++ com.variamos.gui/src/com/variamos/gui/maineditor/MainFrame.java
@@ -47,7 +47,7 @@ public class MainFrame extends JFrame {
 	private boolean showSimulationCustomizationBox = false;
 	private static String variamosVersionNumber = "1.0.1.19";
 	private String variamosVersionName = "1.0 Beta 19";
-	private String variamosBuild = "20161011-1600";
+	private String variamosBuild = "20161020-1730";
 	private String downloadId = "499";
 	private static boolean solverError = false;
 	private static String filesUrl = "";
