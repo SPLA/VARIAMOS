a4d1f812ed3752e5848529f0c1729b6698a21823
jcmunozf <jcmunoz@icesi.edu.co>
2018-08-03 23:09:04 -0500

    FIX: verification operations using meta-models from files

--- com.variamos.dynsup/src/com/variamos/dynsup/model/ModelExpr.java
+++ com.variamos.dynsup/src/com/variamos/dynsup/model/ModelExpr.java
@@ -1213,8 +1213,8 @@ public class ModelExpr implements Serializable, Cloneable {
 			case RIGHTUNIQUEOUTCONVARIABLE:
 			case RIGHTUNIQUEINCCONVARIABLE:
 				if (iter) {
-					elemetType = getSemanticExpression()
-							.getLeftSemanticElement().getIdentifier();
+					//elemetType = getSemanticExpression()
+					//		.getLeftSemanticElement().getIdentifier();
 					if (pos == -1)
 						leftInstElement = elements.get(0);
 					else
